86a371d45c61d2738e0e8965141f86589425c25f
JMARR0W <83468490+JMARR0W@users.noreply.github.com>
2024-10-26 17:05:26 -0700

    attempting to integrate eyemovement in a way that actually interacts with the page

--- eyetracker.js
+++ eyetracker.js
@@ -1,10 +1,29 @@
-window.saveDataAcrossSessions = true
 
-webgazer.setGazeListener(function(data, elapsedTime) {
-	if (data == null) {
-		return;
-	}
-	var xprediction = data.x; //these x coordinates are relative to the viewport
-	var yprediction = data.y; //these y coordinates are relative to the viewport
-	console.log(elapsedTime); //elapsed time is based on time since begin was called
+window.saveDataAcrossSessions = true
+webgazer.showVideoPreview(false)
+webgazer.applyKalmanFilter(true)
+
+webgazer.setGazeListener(function (data, elapsedTime) {
+    if (data == null) {
+        return;
+    }
+    var element_predicted = document.elementFromPoint(data.x, data.y)
+    console.log(element_predicted)
+    if (document.elementFromPoint(data.x, data.y) != null) {
+
+        // Get the element
+        const element = document.elementFromPoint(data.x, data.y);
+
+        // Store the original CSS value
+        const originalOpacity = element.style.opacity;
+
+        // Change the CSS property
+        element.style.opacity = 0.7;
+
+        // Set it back after a delay (e.g., 0.1 seconds)
+        setTimeout(() => {
+            element.style.opacity = originalOpacity;
+        }, 100);
+    }
 }).begin();
+
